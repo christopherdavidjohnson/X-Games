f18ed7c119ffeeb555dd72988710299c5cd51a57
Chris Johnson <studywork@Chriss-MacBook-Air.local>
2021-05-25 11:48:46 +1000

    Nothing to add

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,15 +27,7 @@ puts "#{User.count} users"
 #   t.datetime "updated_at", null: false
 #   t.text "frequency"
 # end
-<<<<<<< HEAD
-Subscriber.destroy_all
-s1 = Subscriber.create :firstname => 'Kimberley', :lastname => "Osborn", :email => 'KO@ga.com', :news => true, :events => true, :skateboarding => true, :snowboarding => true
-s2 = Subscriber.create :firstname => 'Chris', :lastname => "Johnson", :email => 'CJ@ga.com', :news => true, :events => true, :skateboarding => true, :snowboarding => true
-s3 = Subscriber.create :firstname => 'Katie', :lastname => "Lugton", :email => 'KL@ga.com', :news => true, :events => false, :skiing => true, :bmx => true
-s4 = Subscriber.create :firstname => 'William', :lastname => "Stone", :email => 'WS@ga.com', :news => false, :events => true, :skiing => true, :bmx => true
-s5 = Subscriber.create :firstname => 'Mauritz', :lastname => "Erick", :email => 'ME@ga.com', :news => true, :events => true, :motoX => true, :skateboarding => true
-s6 = Subscriber.create :firstname => 'Mary Jane', :lastname => "Delgado", :email => 'MJD@ga.com', :news => true, :events => true, :skiing => true, :snowboarding => true
-=======
+
 
 
 Subscriber.destroy_all
@@ -51,5 +43,5 @@ s5 = Subscriber.create :firstname => 'Mauritz', :lastname => "Erick", :email => 
 
 s6 = Subscriber.create :firstname => 'Mary Jane', :lastname => "Delgado", :email => 'MJD@ga.com', :news => true, :events => true, :skiing => true, :snowboarding => true
 
->>>>>>> 11460c102488b9d069ce1a6829fe496c75e0c1f3
+
 puts "#{User.count} subscribers"
